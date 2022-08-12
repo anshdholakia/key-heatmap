f298fef0f94a1298aa5232c26a79c13fe0daad4c
Ansh Dholakia <anshhiro.dholakia@gmail.com>
2022-08-12 13:43:05 -0400

    simple equation for finding colors

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
-var key_percent = 40 // example data
+var key_percent = 41 // example data
 var colors=['#fff33b', '#fdc70c', '#f3903f', '#ed683c', '#e93e3a'] // 
 
 // since thre are 5 colors, we will divide the percentage by 20 and make the opacity in decimal form of percentage
-var color = colors[Math.round(key_percent/20)];
+// G -> 200 to 1
+// (0, 200)
+// (100, 1)
+// slope: -1.99
+var color = `rgb(255, ${Math.round(200 -1.99*key_percent)}, 1)`;
 
 var element= document.querySelectorAll('div')[3]
 element.style.background = color;
-element.style.opacity = `${key_percent}%`;
